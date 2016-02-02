c90cceece8e88bab28ec697f666299c4ac5d2232
dagut17 <dagutierrez17@gmail.com>
2016-02-01 19:05:10 -0600

    version1.0
    Daniel Gutierrez

---
+++ Hello_Daniel/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Daniel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* my first program in C# */
+            Console.WriteLine("Daniel Gutiérrez");
+            Console.ReadKey();
+        }
+    }
+}
